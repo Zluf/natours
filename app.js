1f0620890892c528f907f69cdd22c8c8206292f6
Slav Konovalov <slav.konovalov@gmail.com>
2025-04-22 17:41:59 +0100

    handle POST requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // middleware for adding body to requests
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
@@ -17,8 +19,28 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-const port = 3000;
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    },
+  );
+});
+
+const port = 5000;
 
 app.listen(port, () => {
-  console.log(`App running on posrt ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
