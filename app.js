8e8cdd0e7c74072efd7edec083456d6d6d58944f
Slav Konovalov <slav.konovalov@gmail.com>
2025-04-22 17:19:02 +0100

    handle GET requests

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    message: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App running on posrt ${port}...`);
+});
