a53bf43afc44a91431880719327de013cdffe478
Slav Konovalov <slav.konovalov@gmail.com>
2025-04-25 10:46:30 +0100

    create simple document to MongoDB database

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# natours
+# Natours
+
+Back-end driven app that serves info on travel tours
+
+## Technologies
+
+- Node.js
+- Express.js
+- MongoDB (NoSQL):
+  - Compass (GUI)
+  - Atlas (database)
+  - Mongoose (object data modeling library)
+
+## Methodology
+
+MVC (Model-View-Controller design pattern)
--- server.js
+++ server.js
@@ -9,16 +9,49 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+// Connect to MongoDB database
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
+    useUnifiedTopology: true,
   })
-  .then((con) => {
-    console.log(con.connections);
-    console.log('DB connection successful!');
-  });
+  .then(() => console.log('DB connection successful!'));
+
+// ▶️ MONGOOSE WorkFlow
+
+// 1. Create a Schema (eq. to TS interface)
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+// 2. Create a Model (eq. to Class) that enforces the Schema's requirements
+const Tour = mongoose.model('Tour', tourSchema);
+
+// 3. Create instance of a Model
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+// 4. Save instance as entry to database
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log('Error 💥:', err));
 
 // console.log(app.get('env'));
 console.log(process.env.NODE_ENV); // process is Node core module
