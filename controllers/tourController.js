267242626e416d6d40ba8fdbe2caed0affb66987
Slav Konovalov <slav.konovalov@gmail.com>
2025-04-27 22:55:42 +0100

    implemented pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,64 +16,78 @@ const checkBody = (req, res, next) => {
 };
 
 const getAllTours = async (req, res) => {
-  // ▪ BUILD QUERY
-
-  // - 1A) Filtering
-  const queryObj = { ...req.query };
-  // /tours?duration[gte]=5 -> { duration: { gte: 5 } }
-
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // - 1B) Advanced filtering
-
-  // we temporarily turn the query object into
-  // string to add "$" to operators (>,>=,<,<=)
-  let queryStr = JSON.stringify(queryObj);
-  console.log('queryStr:', queryStr);
-
-  // Replace: /tours?duration[gte]=5 -> /tours?duration[$gte]=5
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-  // \b - match exacts words: "gte", "gt", "lte", "lt", not just part of word
-  // g - all instances to be replaced
-
-  // Here we declare what we want to fetch
-  // to then use all kinds of query methods
-  // (we don't yet return the promise)
-  let query = Tour.find(JSON.parse(queryStr));
-
-  // - 2) Sorting
-
-  // /tours?sort=-price
-  // would sort documents in descending order of price
-  if (req.query.sort) {
-    // multiple query values e.g /tours?sort=-price,ratingsAverage
-    // are initally read as one string - { sort: '-price,ratingsAverage' }
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  // - 3) Field limiting (projecting)
-
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    query.select(fields);
-  } else {
-    // we exclude that field bc it's no use to the user
-    query = query.select('-__v');
-  }
-
-  // ▪ EXECUTE QUERY
-
-  // Here we actually return the promise
-  const tours = await query;
-  // All the above could be replaced by just const tours = await Tour.find() which
-  // just fetch all the tour entries (documents) but we want things more elaborate
-
-  // ▪ SEND RESPONSE
   try {
+    // ▪ BUILD QUERY
+
+    // - 1A) Filtering
+    const queryObj = { ...req.query };
+    // /tours?duration[gte]=5 -> { duration: { gte: 5 } }
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // - 1B) Advanced filtering
+
+    // we temporarily turn the query object into
+    // string to add "$" to operators (>,>=,<,<=)
+    let queryStr = JSON.stringify(queryObj);
+
+    // Replace: /tours?duration[gte]=5 -> /tours?duration[$gte]=5
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // \b - match exacts words: "gte", "gt", "lte", "lt", not just part of word
+    // g - all instances to be replaced
+
+    // Here we declare what we want to fetch
+    // to then use all kinds of query methods
+    // (we don't yet return the promise)
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // - 2) Sorting
+
+    // /tours?sort=-price
+    // would sort documents in descending order of price
+    if (req.query.sort) {
+      // multiple query values e.g /tours?sort=-price,ratingsAverage
+      // are initally read as one string - { sort: '-price,ratingsAverage' }
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('name');
+    }
+
+    // - 3) Field limiting (projecting)
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query.select(fields);
+    } else {
+      // we exclude that field bc it's no use to the user
+      query = query.select('-__v');
+    }
+
+    // - 4) Pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      // this error would skip the rest of try block moving right to catch block
+      if (skip >= numTours) throw new Error("This page doesn't exist");
+    }
+
+    // ▪ EXECUTE QUERY
+
+    // Here we actually return the promise
+    const tours = await query;
+    // All the above could be replaced by just const tours = await Tour.find() which
+    // just fetch all the tour entries (documents) but we want things more elaborate
+
+    // ▪ SEND RESPONSE
+
     res.status(200).json({
       message: 'success',
       results: tours.length,
@@ -82,7 +96,7 @@ const getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: `Error getting all tours: ${err}`,
+      message: err.message,
     });
   }
 };
