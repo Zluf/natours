5eb6106146b7a8c05433bc0607888e067541bc57
Slav Konovalov <slav.konovalov@gmail.com>
2025-05-04 13:25:25 +0100

    separate error handling during development and production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,40 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    // Operational, trusted error: send msg to client
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or other unknown error
+    console.error('💥 ERROR:', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
 // ERROR-HANDLING MIDDLEWARE - rec-zed by 4 parameters
 module.exports = (err, req, res, next) => {
   // tells us where the line error happened
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if ((process.env.NODE_ENV = 'production')) {
+    sendErrorProd(err, res);
+  }
 };
