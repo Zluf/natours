a0de0d8391ed28ae409f94965eca584beb4cca17
Slav Konovalov <slav.konovalov@gmail.com>
2025-05-04 11:35:03 +0100

    implement global error-handling middleware

--- app.js
+++ app.js
@@ -30,9 +30,28 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't inject ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't inject ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't inject ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // anything passed to next() is an error
+  // it will skip all further middleware
+  // and get straight into error-handling middleware
+  next(err);
+});
+
+// ERROR-HANDLING MIDDLEWARE - rec-zed by 4 parameters
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
